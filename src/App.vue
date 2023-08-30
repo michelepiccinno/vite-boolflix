b2952d1ecf279293144496772d5c535d8b5edc13
michelepiccinno <michele.piccinno@gmail.com>
2023-08-30 13:06:15 +0200

    1.6

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import axios from "axios";
 import Hero from "./components/Hero.vue";
 import TheNavbar from "./components/TheNavbar.vue";
 /*import { store } from "../src/state";*/
-import { onMounted } from "vue";
+
 
 export default {
   components: { 
@@ -20,9 +20,9 @@ export default {
   methods: {
     fetchData(searchText) {
       axios
-        .get("https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query==${searchText}", {
+        .get(`https://api.themoviedb.org/3/search/movie?api_Key=${apiKey}&query==${searchText}`, {
           params: {
-            api_key: "2eb9078bd67d9d05d1087874ecb09554",
+            apikey: "2eb9078bd67d9d05d1087874ecb09554",
             query: searchText,
           },
         })

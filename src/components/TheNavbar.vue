16e7b2c45342715246beadd1548f9b8ea2bf8dd8
michelepiccinno <michele.piccinno@gmail.com>
2023-08-30 11:24:15 +0200

    1.1

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,17 +1,27 @@
 <script>
 export default {
-data() {
-  return {
-    searchText: "",
-  };
-},
-methods: {
-  onEnter() {
-    this.$emit("search", this.searchText);
+  data() {
+    return {
+      searchText: "",
+    };
   },
-},
+  methods: {
+    onEnter() {
+      this.$emit("search", this.searchText);
+    },
+  },
 };
 </script>
+
 <template>
-<input type="text" v-model="searchText" @keyup.enter="OnEnter" />
+  <div class="search-box bg-warning">
+    <input type="text" v-model="searchText" @keyup.enter="OnEnter" />
+  </div>
 </template>
+
+<style>
+.search-box{
+/*  background-color: red;*/
+  height: 50px;
+}
+</style>
